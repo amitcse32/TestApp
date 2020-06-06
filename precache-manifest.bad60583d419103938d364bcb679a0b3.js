b72301876be3cd542bedd40f2f3899fad8c4fa41
apple <apple@CSPC178.local>
2020-06-06 22:48:37 +0530

    Updates

---
+++ precache-manifest.bad60583d419103938d364bcb679a0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4bf24c1a68fddd21388d661cdc0c8e",
+    "url": "/TestApp/index.html"
+  },
+  {
+    "revision": "5f401430a2adf92fd81c",
+    "url": "/TestApp/static/css/2.0fa7cd23.chunk.css"
+  },
+  {
+    "revision": "e3c6f7601927b466ee6b",
+    "url": "/TestApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f401430a2adf92fd81c",
+    "url": "/TestApp/static/js/2.80d1423f.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "/TestApp/static/js/2.80d1423f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c6f7601927b466ee6b",
+    "url": "/TestApp/static/js/main.f4d76e4f.chunk.js"
+  },
+  {
+    "revision": "f1a75c1f9940ffe75f06",
+    "url": "/TestApp/static/js/runtime-main.b5d0fab6.js"
+  }
+]);
